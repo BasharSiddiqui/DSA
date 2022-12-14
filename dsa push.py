6435a00cdf6bc1bb7f14f0da0378e9e97aea37df
WahajAB <wahajahmedbutt@gmail.com>
2022-12-15 03:27:25 +0500

    Suffering

--- dsa push.py
+++ dsa push.py
@@ -5,7 +5,11 @@ from nltk.corpus import stopwords
 directory = r'E:\nela-gt-2021\newsdata'
 directory2 = r'E:\nela-gt-2021\cleanednewsdata'
 folder = os.listdir(directory)
-for filename in folder:
+# Create an empty set to store the words in the lexicon
+lexicon = set()
+index = {}
+for i in range(0,1):
+    filename = folder[i]
     f = os.path.join(directory, filename) 
     with open(f, 'r') as File:
         data = json.load(File)
@@ -14,6 +18,18 @@ for filename in folder:
             i["title"] = [x.lower() for x in i["title"] if (x.isalnum() and x not in stopwords.words('english'))]
             i["content"] = wordpunct_tokenize(i["content"])
             i["content"] = [x.lower() for x in i["content"] if (x.isalnum() and x not in stopwords.words('english'))]
+            # Add the words from the title and content fields to the lexicon
+            lexicon.update(i["title"])
+            lexicon.update(i["content"])
+            for word in lexicon:
+                if word in i["title"]:
+                    index[word] = index.get(word, []) + [i["id"]]
+                if word in i["content"]:
+                    index[word] = index.get(word, []) + [i["id"]]
     F = os.path.join(directory2, filename)
     with open (F, 'w') as FiLe:
-        json.dump(data, FiLe)
+        json.dump(data, FiLe) 
+
+print(lexicon)
+print("YAHALLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+print(index)
