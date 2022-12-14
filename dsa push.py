858a7c121b16e376b866eb7a6f72db972840aba2
WahajAB <wahajahmedbutt@gmail.com>
2022-12-14 22:14:15 +0500

    Test

--- dsa push.py
+++ dsa push.py
@@ -1,1 +1,12 @@
-print("gay")
+import nltk 
+import json 
+import os
+directory = r'E:\nela-gt-2021\newsdata'
+folder = os.listdir(directory)
+for i in range(0,3):
+    filename = folder[i]
+    f = os.path.join(directory, filename) 
+    with open(f, 'r') as File:
+        data = json.load(File)
+        for i in data:
+            print(i["title"])
