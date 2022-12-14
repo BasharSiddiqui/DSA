448f99ebb5fcf126b3cbfa174d34eeaa7fa9726e
WahajAB <wahajahmedbutt@gmail.com>
2022-12-15 01:46:35 +0500

    Test1

--- dsa push.py
+++ dsa push.py
@@ -1,12 +1,19 @@
-import nltk 
+from nltk import *
 import json 
 import os
+from nltk.corpus import stopwords
 directory = r'E:\nela-gt-2021\newsdata'
+directory2 = r'E:\nela-gt-2021\cleanednewsdata'
 folder = os.listdir(directory)
-for i in range(0,3):
-    filename = folder[i]
+for filename in folder:
     f = os.path.join(directory, filename) 
     with open(f, 'r') as File:
         data = json.load(File)
         for i in data:
-            print(i["title"])
+            i["title"] = wordpunct_tokenize(i["title"])
+            i["title"] = [x.lower() for x in i["title"] if (x.isalnum() and x not in stopwords.words('english'))]
+            i["content"] = wordpunct_tokenize(i["content"])
+            i["content"] = [x.lower() for x in i["content"] if (x.isalnum() and x not in stopwords.words('english'))]
+    F = os.path.join(directory2, filename)
+    with open (F, 'w') as FiLe:
+        json.dump(data, FiLe)
