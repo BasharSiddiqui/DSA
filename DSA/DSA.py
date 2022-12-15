8598f24a6faaab0669c436759df4dab4a67fdce9
WahajAB <wahajahmedbutt@gmail.com>
2022-12-15 07:42:50 +0500

    test1

--- DSA/DSA.py
+++ DSA/DSA.py
@@ -1,40 +1,64 @@
-from nltk import wordpunct_tokenize
-import json 
+import json
 import os
+from nltk import wordpunct_tokenize
 from nltk.corpus import stopwords
+from multiprocessing import Process, Pool
+Stopwords = stopwords.words("english")
+class ProcessFile:
+    def __init__(myuwuobject, filename, directory, directory2):
+        super().__init__()
+        myuwuobject.filename = filename
+        myuwuobject.directory = directory
+        myuwuobject.directory2 = directory2
+        myuwuobject.lexicon = set()
+        myuwuobject.index = {}
+
+    def run(myuwuobject):
+        f = os.path.join(myuwuobject.directory, myuwuobject.filename) 
+        with open(f, 'r') as File:
+            data = json.load(File)
+            for i in data:
+                i["title"] = wordpunct_tokenize(i["title"])
+                i["title"] = [x.lower() for x in i["title"] if (x.isalnum() and x not in Stopwords)]
+                i["content"] = wordpunct_tokenize(i["content"])
+                i["content"] = [x.lower() for x in i["content"] if (x.isalnum() and x not in Stopwords)]
+                # Add the words from the title and content fields to the lexicon
+                myuwuobject.lexicon.update(i["title"])
+                myuwuobject.lexicon.update(i["content"])
+                for word in myuwuobject.lexicon:
+                    if word in i["title"]:
+                        myuwuobject.index[word] = myuwuobject.index.get(word, []) + [i["id"]]
+                    if word in i["content"]:
+                        myuwuobject.index[word] = myuwuobject.index.get(word, []) + [i["id"]]
+        F = os.path.join(myuwuobject.directory2, myuwuobject.filename)
+        with open (F, 'w') as FiLe:
+            json.dump(data, FiLe)
+
 directory = r'E:\nela-gt-2021\newsdata'
 directory2 = r'E:\nela-gt-2021\cleanednewsdata'
 folder = os.listdir(directory)
-# Create an empty set to store the words in the lexicon
 lexicon = set()
 index = {}
-for i in range(0,1):
-    filename = folder[i]
-    f = os.path.join(directory, filename) 
-    with open(f, 'r') as File:
-        data = json.load(File)
-        for i in data:
-            i["title"] = wordpunct_tokenize(i["title"])
-            i["title"] = [x.lower() for x in i["title"] if (x.isalnum() and x not in stopwords.words('english'))]
-            i["content"] = wordpunct_tokenize(i["content"])
-            i["content"] = [x.lower() for x in i["content"] if (x.isalnum() and x not in stopwords.words('english'))]
-            # Add the words from the title and content fields to the lexicon
-            lexicon.update(i["title"])
-            lexicon.update(i["content"])
-            for word in lexicon:
-                if word in i["title"]:
-                    index[word] = index.get(word, []) + [i["id"]]
-                if word in i["content"]:
-                    index[word] = index.get(word, []) + [i["id"]]
-    F = os.path.join(directory2, filename)
-    with open (F, 'w') as FiLe:
-        json.dump(data, FiLe) 
+# Create a thread for each file in the folder
+if __name__ == '__main__':
 
-Lexicon = r'C:\Users\wahaj\source\repos\DSA\DSA\Lexicon.json'
-to_write = json.dumps(lexicon)
-with open (Lexicon, 'w') as L:
-    json.dump(lexicon, L)
-Index = r'C:\Users\wahaj\source\repos\DSA\DSA\Index.json'
-to_write = json.dumps(index)
-with open (Index, 'w') as I:
-    json.dump(index, I)
+    files = list(filename for filename in folder)
+    objects = [] 
+    procs = []
+    p = Pool(processes = 10)
+    for i in range(0,4): 
+        objects.append(ProcessFile(folder[i], directory, directory2))
+    proc = p.map(ProcessFile.run, objects)
+    for obj in objects:
+        lexicon.update(obj.lexicon)
+        index.update(obj.index)
+    Lexicon = r'C:\Users\wahaj\source\repos\DSA\DSA\Lexicon.json'
+    lexicon = list(lexicon)
+    to_write = json.dumps(lexicon)
+    with open (Lexicon, 'w') as L:
+        json.dump(lexicon, L)
+
+    Index = r'C:\Users\wahaj\source\repos\DSA\DSA\Index.json'
+    to_write = json.dumps(index)
+    with open (Index, 'w') as I: 
+        json.dump(index, I)
