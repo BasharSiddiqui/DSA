3899b41f1fb0e8fd361d7f28773c3e6111aa60b9
WahajAB <wahajahmedbutt@gmail.com>
2022-12-17 20:24:16 +0500

    F

--- DSA/DSA.py
+++ DSA/DSA.py
@@ -1,47 +1,52 @@
 import json
 import os
 from math import ceil
-from nltk import word_tokenize
+from nltk import wordpunct_tokenize, WordNetLemmatizer
 from nltk.corpus import stopwords
 from multiprocessing import Pool, cpu_count
-from nltk.stem import WordNetLemmatizer 
+
 lemmatizer = WordNetLemmatizer()
 Stopwords = stopwords.words("english")
 path = os.getcwd()
 directory = os.path.join(path, "Uncleaned")
 directory2 =os.path.join(path, "Cleaned")
 folder2 = os.listdir(directory2)
-folder = os.listdir(directory)
+
+folder = os.listdir(directory)    
 class ProcessFile:
-    def __init__(myuwuobject, filename, directory, directory2):
+    def __init__(myuwuobject, filename, directory, directory2, index):
         myuwuobject.filename = filename
         myuwuobject.directory = directory
         myuwuobject.directory2 = directory2
         myuwuobject.lexicon = set()
         myuwuobject.inv_index = {}
+        myuwuobject.i = index
     def run(myuwuobject):
+        j = 0
         if myuwuobject.filename in folder2:
-            return myuwuobject
+            return [myuwuobject.lexicon, myuwuobject.inv_index]
         f = os.path.join(myuwuobject.directory, myuwuobject.filename) 
         with open(f, 'r') as File:
             data = json.load(File)
-            for i in data:        
-                i["title"] = word_tokenize(i["title"])
-                i["title"] = [lemmatizer.lemmatize(x.lower()) for x in i["title"] if (x.isalnum() and x not in Stopwords)]
-                i["content"] = word_tokenize(i["content"])
-                i["content"] = [lemmatizer.lemmatize(x.lower()) for x in i["content"] if (x.isalnum() and x not in Stopwords)]
+            for i in data:  
+                i["doc_ID"] = str(myuwuobject.i) + "-" + str(j)
+                j += 1
+                i["title"] = wordpunct_tokenize(i["title"])
+                i["title"] = [lemmatizer.lemmatize(x.lower()) for x in i["title"] if (x.isalnum() and x.lower() not in Stopwords)]
+                i["content"] = wordpunct_tokenize(i["content"])
+                i["content"] = [lemmatizer.lemmatize(x.lower()) for x in i["content"] if (x.isalnum() and x.lower() not in Stopwords)]
                 # Add the words from the title and content fields to the lexicon
                 myuwuobject.lexicon.update(i["title"])
                 myuwuobject.lexicon.update(i["content"])
                 for word in myuwuobject.lexicon:
                     if word in i["title"]:
-                        myuwuobject.inv_index[word] = myuwuobject.inv_index.get(word, []) + [i["id"]]
+                        myuwuobject.inv_index[word] = myuwuobject.inv_index.get(word, []) + [i["doc_ID"]]
                     elif word in i["content"]:
-                        myuwuobject.inv_index[word] = myuwuobject.inv_index.get(word, []) + [i["id"]]
+                        myuwuobject.inv_index[word] = myuwuobject.inv_index.get(word, []) + [i["doc_ID"]]
         F = os.path.join(myuwuobject.directory2, myuwuobject.filename)
         with open (F, 'w') as FiLe:
             json.dump(data, FiLe)
-        return myuwuobject
+        return [myuwuobject.lexicon, myuwuobject.inv_index]
 #Main
 if __name__ == '__main__':
     with open(os.path.join(path, "Lexicon.json"), 'r') as File:
@@ -53,14 +58,16 @@ if __name__ == '__main__':
     inv_index = eval(inv_index)
     objects = [] 
     workers = cpu_count()-1
+    if workers == 0:
+        workers = 1
     p = Pool(workers)
-    for i in range(0,10): 
-        objects.append(ProcessFile(folder[i], directory, directory2))
+    for i in range(0,4): 
+        objects.append(ProcessFile(folder[i], directory, directory2, i))
     chunk = ceil(len(objects)/workers)
-    proc = p.imap_unordered(ProcessFile.run, objects, chunksize = chunk)
-    for obj in proc:
-        lexicon.update(obj.lexicon)
-        inv_index.update(obj.inv_index)    
+    proc = p.imap_unordered(ProcessFile.run, objects, chunksize = chunk) 
+    for p in proc: 
+        lexicon.update(p[0])
+        inv_index.update(p[1])
     lexicon = list(lexicon)
     to_write = json.dumps(lexicon)
     with open (os.path.join(path, "Lexicon.json"), 'w') as L:
